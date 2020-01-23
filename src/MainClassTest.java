f9a8ac72ed82e911c26b3bd212b6e4bd6291f0fd
yaroslavkorotyshov <ydkorotyshov@gmail.com>
2020-01-23 21:41:31 +0300

    Test 1

---
+++ src/MainClassTest.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass{
+    @Test
+    public void testGetLocalNumber(){
+        Assert.assertTrue("Fails - this number is not 14", getLocalNumber() == 14);
+    }
+}
