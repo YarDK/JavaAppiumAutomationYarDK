369595c048516aeee39fef1976344595e7a87dc6
yaroslavkorotyshov <ydkorotyshov@gmail.com>
2020-01-23 21:48:14 +0300

    Test 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,8 +2,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest extends MainClass{
+    
+    /*
     @Test
     public void testGetLocalNumber(){
         Assert.assertTrue("Fails - this number is not 14", getLocalNumber() == 14);
+    }
+    */
+
+    @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("Fails - this number is not greater", getClassNumber() > 45);
     }
 }
