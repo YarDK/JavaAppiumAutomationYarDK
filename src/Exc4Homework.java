9bf30e4337d272dc03a6da55d0a09887b5876bc3
ydkor <ydkorotyshov@gmailcom>
2020-02-15 13:27:53 +0300

    Ex6: Тест: assert title. Finish

--- src/Exc4Homework.java
+++ src/Exc4Homework.java
@@ -3,19 +3,18 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
 import java.net.URL;
-import java.util.ArrayList;
+
 
 public class Exc4Homework {
     private AppiumDriver driver;
@@ -23,7 +22,6 @@ public class Exc4Homework {
     @Before
     public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        //String path_to_apk = new File("org.wikipedia.apk").getAbsolutePath();
 
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName", "AndroidTestDevice");
@@ -31,8 +29,8 @@ public class Exc4Homework {
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "C:\\Users\\yako\\IdeaProjects\\JavaAppiumAutomationYarDK\\apks\\org.wikipedia.apk");
-        //capabilities.setCapability("app", "D:\\IdeaProjects\\JavaAppiumAutomationYarDK\\apks\\org.wikipedia.apk");
+        //capabilities.setCapability("app", "C:\\Users\\yako\\IdeaProjects\\JavaAppiumAutomationYarDK\\apks\\org.wikipedia.apk");
+        capabilities.setCapability("app", "D:\\IdeaProjects\\JavaAppiumAutomationYarDK\\apks\\org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
@@ -43,15 +41,13 @@ public class Exc4Homework {
     }
 
     /*
-    Написать тест, который:
-    1. Сохраняет две статьи в одну папку
-    2. Удаляет одну из статей
-    3. Убеждается, что вторая осталась
-    4. Переходит в неё и убеждается, что title совпадает
+    Написать тест, который открывает статью и убеждается, что у нее есть элемент title.
+    Важно: тест не должен дожидаться появления title, проверка должна производиться сразу.
+    Если title не найден - тест падает с ошибкой. Метод можно назвать assertElementPresent.
     */
 
     @Test
-    public void testSavingTwoArticles()
+    public void testAssertTitle()
     {
         //Нажать на поиск
         String input_field_search_wikipedia_by_id = "org.wikipedia:id/search_container";
@@ -78,186 +74,9 @@ public class Exc4Homework {
                 5
         );
 
-        //Сохранить в переменную название статьи и сабтитл
-        String title_of_article_java_by_id = "org.wikipedia:id/view_page_title_text";
-        String subtitle_of_article_java_by_id = "org.wikipedia:id/view_page_subtitle_text";
+        // Посик title
+        assertElementPresent(By.id("org.wikipedia:id/view_page_title_text"));
 
-        waitForElementPresents(
-                By.id(title_of_article_java_by_id),
-                "Element 'title_of_article_java_by_id' can not find",
-                10);
-
-        String title_of_article_java = driver.findElement(By.id(title_of_article_java_by_id)).getAttribute("text");
-        String subtitle_of_article_java = driver.findElement(By.id(subtitle_of_article_java_by_id)).getAttribute("text");
-
-        //Нажать меню
-        String more_options_button_by_xpath = "//android.support.v7.widget.LinearLayoutCompat//android.widget.ImageView[@content-desc='More options']";
-        waitForElementAndClick(
-                By.xpath(more_options_button_by_xpath),
-                "Button 'More options' can not find.",
-                5
-        );
-
-        //Нажать сохранить статью
-        waitingForElement(3000);
-        String function_add_to_reading_list_by_xpath = "//*[@text='Add to reading list']";
-        waitForElementAndClick(
-                By.xpath(function_add_to_reading_list_by_xpath),
-                "Element 'Add to reading list' can not find.",
-                5
-        );
-        //Прокликать ОК
-        String onboarding_button__got_it_by_id = "org.wikipedia:id/onboarding_button";
-        waitForElementAndClick(
-                By.id(onboarding_button__got_it_by_id),
-                "Element 'onboarding_button 'Got it'' can not find.",
-                5
-        );
-
-        //Нужно создать папку, но в кейсе и в реале старт этой фичи отличается, нужно разобраться
-        //Сохдать переменную с названием папки
-        //Ввести название папки используя переменную
-        String text_input_field_by_id = "org.wikipedia:id/text_input";
-        String name_folder = "Learning Programming";
-        waitForElementAndClear(
-                By.id(text_input_field_by_id),
-                "Element 'text_input_field' can not find.",
-                5
-        );
-        waitForElementAndSendKeys(
-                By.id(text_input_field_by_id),
-                name_folder,
-                "Element 'text_input_field' can not find.",
-                3
-        );
-
-        //Подтвердить создание
-        waitForElementAndClick(
-                By.xpath("//*[@text='OK']"),
-                "Element 'OK' can not find.",
-                5
-        );
-
-        //Закрыть статью
-        String button_navigation_cross_by_xpath = "//android.widget.ImageButton[@content-desc='Navigate up']";
-        waitForElementAndClick(
-                By.xpath(button_navigation_cross_by_xpath),
-                "Element 'Navigate up 'X'' can not find.",
-                5
-        );
-
-        //Нажать на поиск
-        waitForElementAndClick(
-                By.id(input_field_search_wikipedia_by_id),
-                "Element 'input_field_search_wikipedia_by_id' can not find",
-                5
-        );
-
-        //Ввести Python
-        waitForElementAndSendKeys(
-                By.id(input_field_search_by_id),
-                "Python",
-                "Element 'input_field_search' can not find",
-                5
-        );
-
-        //Открыть статью, где описывается, что это язык программирования
-
-        waitForElementAndClick(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Python (programming language)']"),
-                "Cannot find result with text 'Java (programming language)'.",
-                5
-        );
-
-        //Сохранить в переменную название статьи и сабтитл
-        String title_of_article_python_by_id = "org.wikipedia:id/view_page_title_text";
-        String subtitle_of_article_python_by_id = "org.wikipedia:id/view_page_subtitle_text";
-
-        waitForElementPresents(
-                By.id(title_of_article_python_by_id),
-                "Element 'title_of_article_python_by_id' can not find",
-                5);
-
-        String title_of_article_python = driver.findElement(By.id(title_of_article_python_by_id)).getAttribute("text");
-
-        //Нажать меню
-        waitForElementAndClick(
-                By.xpath(more_options_button_by_xpath),
-                "Button 'More options' can not find.",
-                5
-        );
-
-        //Нажать сохранить статью
-        waitingForElement(3000);
-        waitForElementAndClick(
-                By.xpath(function_add_to_reading_list_by_xpath),
-                "Element 'Add to reading list' can not find.",
-                5
-        );
-
-        //Выбрать папку, ранее созданную
-        String folder_with_articles_by_xpath = "//*[@resource-id='org.wikipedia:id/item_container']//*[@text='" + name_folder + "']";
-        waitForElementAndClick(
-                By.xpath(folder_with_articles_by_xpath),
-                "Folder or element '"+ name_folder +"' can not find.",
-                5
-        );
-
-        //Закрыть статью
-        waitForElementAndClick(
-                By.xpath(button_navigation_cross_by_xpath),
-                "Element 'Navigate up 'X'' can not find.",
-                5
-        );
-
-        //Открыть окно Мой лист
-        waitForElementAndClick(
-                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
-                "Element 'My lists' can not find.",
-                5
-        );
-
-        //Открыть папку, ранее созданную
-        waitingForElement(3000);
-        waitForElementAndClick(
-                By.xpath("//*[@text='" + name_folder + "']"),
-                "Folder 'Learning Programming' can not find.",
-                5
-        );
-
-        //Дождаться загрузку элементов
-        waitForElementPresents(
-                By.xpath("//*[@text='" + name_folder + "']"),
-                "Name folder 'Learning Programming' can not find."
-        );
-
-        //Удалить статью, который про Python
-        swipeElementToLeft(
-                By.xpath("//*[@text='" + title_of_article_python + "']"),
-                "Element with text '" + title_of_article_python + "' can not find."
-        );
-        //Проверить, что вторая статья осталась
-        waitForElementPresents(
-                By.xpath("//*[@text='" + title_of_article_java + "']"),
-                "Element with text '" + title_of_article_java + "' can not find."
-        );
-        //Открыть ее
-        waitForElementAndClick(
-                By.xpath(article_about_java_programming_language),
-                "Cannot find article with text 'Java (programming language)'.",
-                5
-        );
-        //Проверить, что эта статья про JAva
-        WebElement title_element = waitForElementPresents(
-                By.id("org.wikipedia:id/view_page_title_text"),
-                "Element 'view_page_title_text' not found",
-                5
-        );
-        Assert.assertEquals(
-                "Article is not about 'Java (programming language)'",
-                title_element.getAttribute("text"),
-                "Java (programming language)"
-        );
 
     }
 
@@ -358,6 +177,14 @@ public class Exc4Homework {
             Thread.sleep(timeForWaiting);
         }catch (Exception e){
             System.out.println(e);
+        }
+    }
+
+    protected void assertElementPresent(By by){
+        try {
+            driver.findElement(by);
+        } catch (NoSuchElementException e){
+            throw new AssertionError("Element '" + by.toString() + "' not found");
         }
     }
 
